e86ea1da1f1add792299b7bff8ce060808e865b3
ShunKUN1182 <2250378@ecc.ac.jp>
2025-10-30 23:53:38 +0900

    モーダル試行錯誤

--- js/test02.js
+++ js/test02.js
@@ -1,16 +1,35 @@
-const imgs = document.querySelectorAll(".short_img");
+const shortImgs = document.querySelectorAll(".short_img");
 const modalBack = document.querySelector(".modalBack");
-const modalImg = document.querySelector("img")
-console.log(imgs);
+const modalImg = document.querySelector(".modalBack > picture > img");
+let modalBox = [];
+console.log(modalBack);
 
+// console.log(shortImgs , modalBack);
 
-for(let i = 1; i++; i < imgs.length + 1){
-    imgs[i].addEventListener("click" , ()=>{
-        modalImg.innerHTML = `<img src="images/img_test0${i}_m.jpg">`
+shortImgs.forEach(e => {
+    e.addEventListener("click" , ()=>{
+        const imageFileName = e.querySelector("img").src.split("images/");
+        const addImage = imageFileName[1].replace("s.jpg" , "m.jpg");
+        console.log(addImage);
+        modalImg.innerHTML = `<img src="images/${addImage}">`
         modalBack.style.display = "flex";
-    }
-)} 
+    })
+});
 
-modalBack.addEventListener("click" , ()=>{
-    modalBack.style.display = "none";
-})
+
+
+// for(let i = 1; i++; i < shortImgs.length + 1){
+//     shortImgsmgs[i].addEventListener("click" , ()=>{
+//         modalImg.innerHTML = `<img src="images/img_test0${i}_m.jpg">`
+//         modalBack.style.display = "flex";
+//     }
+// )} 
+
+// modalBack.addEventListener("click" , ()=>{
+//     modalBack.style.display = "none";
+// })
+
+// shortImgs.forEach(e => {
+//     e.addEventListener("click" , ()=>{
+//     })
+// });
