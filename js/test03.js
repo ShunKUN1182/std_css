b0acfec231ed6131d6ad29872f1b291f31202bff
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-05 20:00:43 +0900

    asyncがむずい

--- js/test03.js
+++ js/test03.js
@@ -1,12 +1,33 @@
 const shortImgs = document.querySelectorAll(".short_img");
 const modalBack = document.querySelector(".modalBack");
 const modalImg = document.querySelector(".modalBack > picture");
-let modalBox = [];
+
+const animation = [
+    { opacity: "0"} , 
+    { opacity: "1"} ,
+];
+
+const reverseAnimation = [
+    { opacity: "1"} , 
+    { opacity: "0"} ,
+];
+
+
+const modalBackAnimationTiming = {
+    duration: 500,
+    iteration: 1,
+};
+
+const modalImgAnimationTiming = {
+    duration: 2000,
+    iteration: 1,
+};
 
 const modalDiv = document.createElement("div");
 modalDiv.classList.add("modalBack");
 const modalPicture = document.createElement("picture");
 const modalBackImg = document.createElement("img");
+
 
 shortImgs.forEach(e => {
     e.addEventListener("click" , {target: e , handleEvent: showModal})
@@ -19,10 +40,31 @@ function showModal() {
     modalPicture.append(modalBackImg);
     modalDiv.append(modalPicture);
     document.body.append(modalDiv);
-    modalDiv.addEventListener("click" , closeModal)
-}
+    modalBack.animate(animation, modalBackAnimationTiming);
+    modalBackImg.animate(animation, modalImgAnimationTiming);
+    modalDiv.addEventListener("click" , closeModal);
+};
 
 function closeModal(event) {
-    if(event.target != modalDiv) return;            
+    if(event.target != modalDiv) return;
+    modalBack.animate(reverseAnimation, modalBackAnimationTiming)
     document.querySelector(".modalBack").remove();
+};
+
+
+async function calcGo() {
+
+    const calc = new Promise((resolve)=>{
+        console.log("start!!");
+        
+    setTimeout(()=>{
+        const result = 100+1000;
+        resolve(result);
+    },1000)
+});
+
+console.log(await calc);
+
 }
+
+calcGo();
