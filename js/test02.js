2e63e29bbfa2b31639fe26b263360aa2a985ff43
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-05 16:31:40 +0900

    test02完成

--- js/test02.js
+++ js/test02.js
@@ -1,8 +1,8 @@
 const shortImgs = document.querySelectorAll(".short_img");
 const modalBack = document.querySelector(".modalBack");
-const modalImg = document.querySelector(".modalBack > picture > img");
+const modalImg = document.querySelector(".modalBack > picture");
 let modalBox = [];
-console.log(modalBack);
+console.log(modalBack.contains(modalImg));
 
 // console.log(shortImgs , modalBack);
 
@@ -16,6 +16,13 @@ shortImgs.forEach(e => {
     })
 });
 
+modalBack.addEventListener("click" , (eve)=>{
+    if(eve.target != modalBack) return;
+    
+    // console.log(modalBack.contains(eve.target));
+    
+    modalBack.style.display = "none";
+})
 
 
 // for(let i = 1; i++; i < shortImgs.length + 1){
